8c824844ee4cf2d614b513a98b60d36e25cfd820
Migz Estrella <migz.zgim@gmail.com>
2020-03-28 11:50:45 +0800

    Adds unit tests for araw.py.

---
+++ tests/araw/test_araw.py
@@ -0,0 +1,29 @@
+from ultraboost.paskoba.lib.araw import Araw
+import unittest
+import datetime
+
+
+class TestAraw(unittest.TestCase):
+
+    def test_get_current_date_with_tz_return_type(self):
+        """
+        Tests if the return type is datetime.datetime
+        :return:
+        """
+        a = Araw()
+        self.assertTrue(isinstance(a.get_current_date_with_tz('Asia/Manila'),
+                                   datetime.datetime))
+
+
+    def test_get_simple_manila_date_return_type(self):
+        """
+        Tests if the return type is string
+        :return:
+        """
+        a = Araw()
+        self.assertTrue(isinstance(a.get_simple_manila_date(),
+                                   str))
+
+
+if __name__ == '__main__':
+    unittest.main()
