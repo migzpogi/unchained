6b8ddc9920c5931c90c2d729be1600e1599b3e12
Migz Estrella <migz.zgim@gmail.com>
2020-04-01 17:49:04 +0800

    Adds main view for ilan app, a sample static image and the index.html

--- ultraboost/ilan/views.py
+++ ultraboost/ilan/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    context = {}
-    return render(request, 'ilan/index.html', context)
+    if request.method == 'POST':
+        data = request.POST.get('textfield', None)
+        x = compute(int(data), 10000)
+        context = {
+            # always convert to int
+            'amount': x,
+            'rows': range(x),
+            'amount_text': f"{x} Ultraboost(s) 'yan!"
+        }
+        return render(request, 'ilan/index.html', context)
+
+    return render(request, 'ilan/index.html')
+
+
+def compute(dividend, divisor):
+    return round(dividend/divisor)
