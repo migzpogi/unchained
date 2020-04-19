57dbdcb6c82a34b00d9e650eeb53b082d8219b90
Migz Estrella <migz.zgim@gmail.com>
2020-04-19 13:58:12 +0800

    Modifies the way how input is retrieved from the user. It now takes it as string and then converts it to float.

--- ultraboost/ilan/lib/ilan.py
+++ ultraboost/ilan/lib/ilan.py
@@ -4,9 +4,37 @@ class Ilan:
     def get_qty(total_amount, price):
         """
         Divides the total amount by its price to get the quantity
-        :param total_amount:
-        :param price:
-        :return:
+        :param total_amount: number to be divided
+        :param price: number used to divide
+        :return: rounded down total_amount/price
         """
 
         return round(total_amount/price)
+
+    @staticmethod
+    def is_string_number_valid(number: str):
+        """
+        Checks if the given number is valid
+        :param str number: input given by the user
+        :return:
+        """
+
+        try:
+            stripped_number = Ilan().__remove_commas(number)
+            return float(stripped_number)
+        except AttributeError:
+            print('Please make sure you provided int')
+            return False
+        except ValueError:
+            print('Please check correct format. No letters, multiple decimal points, special characters')
+            return False
+
+    @staticmethod
+    def __remove_commas(number):
+        """
+        Removes commas
+        :param str number:
+        :return:
+        """
+
+        return number.replace(',', '')
--- ultraboost/ilan/views.py
+++ ultraboost/ilan/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .lib.ilan import Ilan
 
 
 def index(request):
     if request.method == 'POST':
         data = request.POST.get('textfield', None)
-        x = compute(int(data), 10000)
-        context = {
-            # always convert to int
-            'amount': x,
-            'rows': range(x),
-            'amount_text': f"{x} Ultraboost(s) 'yan!"
-        }
-        return render(request, 'ilan/index.html', context)
+        clean_data = Ilan().is_string_number_valid(data)
+
+        if clean_data:
+            x = Ilan().get_qty(float(clean_data), 10000)
+            context = {
+                # always convert to int
+                'amount': x,
+                'rows': range(x),
+                'amount_text': f"{x} Ultraboost(s) 'yan!"
+            }
+            return render(request, 'ilan/index.html', context)
 
     return render(request, 'ilan/index.html')
-
-
-def compute(dividend, divisor):
-    return round(dividend/divisor)
