c4bb9397cf97d54b8283b91f3e3c5edafe97d71d
Ubuntu-NZXT-VMW <migz.zgim@gmail.com>
2020-06-08 09:33:31 +0800

    Adds solution to Euler 001

--- ultraboost/euler/lib/euler_001.py
+++ ultraboost/euler/lib/euler_001.py
@@ -1,2 +1,18 @@
-def hello():
-    return "world"
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+233168
+"""
+
+total = 0
+for num in range(1, 1000):
+    if num % 3 == 0 or num % 5 == 0:
+        total += num
+
+print(total)
+
+# one liner alternate answer
+print(sum([num for num in range(1, 1000) if (num % 3 == 0 or num % 5 == 0)]))
+
