2c80e4a582376c8342ff8179d1d9fb8a4ae3455b
Migz <migz.zgim@gmail.com>
2020-06-22 22:22:19 +0800

    Starts Euler 017

---
+++ ultraboost/euler/lib/euler_017.py
@@ -0,0 +1,23 @@
+def to_words(n):
+    if n == 0:
+        return "zero"
+    if n == 1:
+        return "one"
+    if n == 2:
+        return "two"
+    if n == 3:
+        return "three"
+    if n == 4:
+        return "four"
+    if n == 5:
+        return "five"
+    if n == 6:
+        return "six"
+    if n == 7:
+        return "seven"
+    if n == 8:
+        return "eight"
+    if n == 9:
+        return "nine"
+    if n == 10:
+        return "ten"
