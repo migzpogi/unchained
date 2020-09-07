6be6aabe6435a0203c4d5c1e68f5a8baaf9e4da1
Migz <migz.zgim@gmail.com>
2020-09-07 10:07:50 +0800

    Finishes Euler 046

--- ultraboost/euler/lib/euler_046.py
+++ ultraboost/euler/lib/euler_046.py
@@ -1,4 +1,21 @@
+from leonhard.leonhard import is_prime
+import itertools
+
+
 def twice_square(n):
     return 2 * (n**2)
 
-print(7+twice_square(1))
+
+for n in itertools.count(1):
+    if n % 2 == 0:
+        continue
+    if not is_prime(n):
+        for i in itertools.count(1):
+            x = twice_square(i)
+            if x < n:
+                if is_prime(n-x):
+                    # print(f"{n} = {n-x} + (2 x ({i ** 2}))")
+                    break
+            else:
+                print(n)
+                break
