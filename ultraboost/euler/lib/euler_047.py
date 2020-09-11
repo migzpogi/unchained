9b6813344a7797172ac355e9f250e3fb16fb4ea8
Migz <migz.zgim@gmail.com>
2020-09-11 21:25:48 +0800

    More code in 47

--- ultraboost/euler/lib/euler_047.py
+++ ultraboost/euler/lib/euler_047.py
@@ -1,3 +1,14 @@
-from leonhard.leonhard import get_factors_of_positive_integer
+from leonhard.leonhard import get_factors_of_positive_integer, is_prime
 
-print(get_factors_of_positive_integer(644))
+
+def remove_one_and_last(n):
+    return n[1:-2]
+
+y = get_factors_of_positive_integer(646)
+
+
+print(remove_one_and_last(y))
+# x = map(is_prime, y)
+#
+# for z in x:
+#     print(z)
