93d0abdd0085942ae693ed5e8d59b5bf39529ef8
Migz <migz.zgim@gmail.com>
2020-09-12 23:40:59 +0800

    Still working on 047

--- ultraboost/euler/lib/euler_047.py
+++ ultraboost/euler/lib/euler_047.py
@@ -1,13 +1,32 @@
 from leonhard.leonhard import get_factors_of_positive_integer, is_prime
 
 
+def is_not_prime(n):
+    return not is_prime(n)
+
+
 def remove_one_and_last(n):
     return n[1:-2]
 
-y = get_factors_of_positive_integer(646)
+
+factors = remove_one_and_last(get_factors_of_positive_integer(646))
+primes = list(filter(is_prime, factors))
+composites = list(filter(is_not_prime, factors))
+
+# print(factors)
+# print(primes)
+# print(composites)
+
+buffer = []
+if composites:
+    for c in composites:
+        print(c)
+        temp = get_factors_of_positive_integer(c)
+        print(temp)
+        primes.extend(list(filter(is_prime, temp)))
+        buffer.extend(list(filter(is_not_prime, temp)))
 
 
-print(remove_one_and_last(y))
 # x = map(is_prime, y)
 #
 # for z in x:
