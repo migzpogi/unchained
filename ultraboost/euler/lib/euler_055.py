891d2eb90bc896c487941b9e7662b991c26d7557
Migz <migz.zgim@gmail.com>
2020-10-01 22:28:39 +0800

    Continues 055

--- ultraboost/euler/lib/euler_055.py
+++ ultraboost/euler/lib/euler_055.py
@@ -4,7 +4,28 @@ def is_palindrome(n):
     else:
         return False
 
+
+def reverse_add(n):
+    reversed = int(str(n)[::-1])
+    return n + reversed
+
+
+def get_palindrome(n):
+    count = 1
+    while True:
+        p = reverse_add(n)
+        if is_palindrome(p):
+            return p, count
+        n = p
+        count += 1
+
+
 print(is_palindrome(1))
 print(is_palindrome(12))
 print(is_palindrome(123321))
 
+print(reverse_add(47))
+print(reverse_add(349))
+
+print(get_palindrome(47))
+print(get_palindrome(349))
