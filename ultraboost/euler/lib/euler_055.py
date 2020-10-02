0fa5340816402d5bd0c70975da7a8737b3d22560
Migz <migz.zgim@gmail.com>
2020-10-02 21:16:23 +0800

    Finishes Euler 055

--- ultraboost/euler/lib/euler_055.py
+++ ultraboost/euler/lib/euler_055.py
@@ -14,18 +14,30 @@ def get_palindrome(n):
     count = 1
     while True:
         p = reverse_add(n)
+        if count > 50:
+            return -1, -1
         if is_palindrome(p):
             return p, count
         n = p
         count += 1
 
 
-print(is_palindrome(1))
-print(is_palindrome(12))
-print(is_palindrome(123321))
+# print(is_palindrome(1))
+# print(is_palindrome(12))
+# print(is_palindrome(123321))
+#
+# print(reverse_add(47))
+# print(reverse_add(349))
+#
+# print(get_palindrome(47))
+# print(get_palindrome(349))
+# print(get_palindrome(196))
 
-print(reverse_add(47))
-print(reverse_add(349))
+lychrel = []
+for i in range(1, 10000):
+    x = get_palindrome(i)
+    if x[0] == -1:
+        lychrel.append(i)
 
-print(get_palindrome(47))
-print(get_palindrome(349))
+print(lychrel)
+print(len(lychrel))
