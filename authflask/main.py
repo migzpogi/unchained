214481970047045a27bbeb81f9b2a20d638211bc
Migz Estrella <migz.zgim@gmail.com>
2021-05-16 21:35:14 +0800

    Adds stub api

--- authflask/main.py
+++ authflask/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
 from flask_login import login_required, current_user
 from . import db
 
@@ -12,3 +12,16 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/api/v1/stub', methods=['GET', 'POST'])
+@login_required
+def stub():
+    sample = {
+        "LAL": 100,
+        "DEN": 89
+    }
+
+    if request.method == 'GET':
+        return jsonify(sample)
+    else:
+        return jsonify(sample)
