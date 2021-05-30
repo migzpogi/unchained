d9756de3b2938cbe9bd7949b393df94c848c659b
Migz Estrella <migz.zgim@gmail.com>
2021-05-30 22:58:35 +0800

    Starts juliandatediff

---
+++ ex/juliandatediff.py
@@ -0,0 +1,20 @@
+"""
+Given two dates in string format:
+    2019-01-01
+    2019-01-20
+
+Find the difference between them without using the datetime library
+"""
+
+def get_julian_date(date):
+    """
+    Gets the Julian date
+
+    Args:
+        date str: Date in string format YYYY-MM-DD (2019-01-20)
+
+    Returns int: Julian date
+
+    """
+
+    
