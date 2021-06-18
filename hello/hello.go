68f476893ada9c3b0c5f8162f3777986da328b19
Migz Estrella <migz.zgim@gmail.com>
2021-06-18 18:58:04 +0800

    Adds error logging

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,18 @@ package main
 
 import(
 	"fmt"
+	"log"
 	"example.com/greetings"
 )
 
 func main(){
-	message := greetings.Hello("Miguel")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
