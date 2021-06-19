5f1513b68c5b26d53bc2072dba110255b5135b84
Migz Estrella <migz.zgim@gmail.com>
2021-06-19 21:49:57 +0800

    Adds random func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,27 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -10,7 +10,7 @@ func main(){
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Miguel")
 	if err != nil {
 		log.Fatal(err)
 	}
